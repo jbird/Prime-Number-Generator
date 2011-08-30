766abca4451d3e4017832f803a83569365c9c7e4
John Bird <johnrb@gmail.com>
2011-08-30 17:42:49 +0100

    Added licensing

--- PrimeNumberGenerator/Form1.cs
+++ PrimeNumberGenerator/Form1.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Prime Number Generator - A prime number generator utility.
+ * Copyright (C) 2008-2011 John Bird <https://github.com/jbird/Prime-Number-Generator>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
--- PrimeNumberGenerator/Primes.cs
+++ PrimeNumberGenerator/Primes.cs
@@ -1,13 +1,19 @@
-﻿/**
- * Primes.cs
- * Copyright(c) 2008-2009 John Bird.
- * Date(last modified): 24 October, 2009.
- * 
- * A Generic class for various prime number related operations
- * such as generating small prime lists, large pseudorandom prime numbers
- * and testing primality.
- * 
- * Uses Bouncy Castle BigInteger class.
+﻿/*
+ * Prime Number Generator - A prime number generator utility.
+ * Copyright (C) 2008-2011 John Bird <https://github.com/jbird/Prime-Number-Generator>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
